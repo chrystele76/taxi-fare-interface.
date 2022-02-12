26ba9446ca58418deefaf0eeada4ce63a286235d
Chrystele Funk <91123507+chrystele76@users.noreply.github.com>
2022-02-12 13:31:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyeXRzZWxlIiwiYSI6ImNremp0Y2ZzbDBrM2kyc2tlNHdjYjRqNG0ifQ.fPK274Xr3_-KG_4YKRsnTQ';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
